db89c0d6a2285eb9c68d40a71999eca684fc732b
jeff <jeffyan167@gmail.com>
2024-09-30 16:46:34 +0800

    代码的初始提交, 只有vue3的代码

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hello Vue 3 + TypeScript + Vite</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<script lang="ts">
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style scoped>
+
+</style>
