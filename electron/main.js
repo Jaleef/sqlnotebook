2d0c374c535ef997bf27312909c43e9001ba7915
jeff <jeffyan167@gmail.com>
2024-10-04 18:39:06 +0800

    添加预渲染脚本preload.js

--- electron/main.js
+++ electron/main.js
@@ -1,18 +1,26 @@
 // const { app, BrowserWindow } = require('electron');
 import { app, BrowserWindow } from 'electron';
 
+// 创建一个窗口
 const createWindow = () => {
+    // 一个window对象
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        // 配置渲染进程的环境选项
         webPreferences: {
-        nodeIntegration: true,
+            // 预加载脚本的路径
+            preload: path.join(__dirname, 'preload.js'),
         },
     });
     
-    win.loadFile('./electron/index.html');
+    // 加载index.html
+    // win.loadFile('./electron/index.html');
+
+    win.loadURL('http://localhost:3001');
 }
 
+// 监听app的ready事件
 app.whenReady().then(() => {
     createWindow();
 
@@ -23,6 +31,7 @@ app.whenReady().then(() => {
     });
 })
 
+// 监听app的window-all-closed事件
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
