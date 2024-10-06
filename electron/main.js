584203bf2edfede65912e46a0d5522efac6e8f4c
jeff <jeffyan167@gmail.com>
2024-10-06 19:16:21 +0800

    完成了vue3运行到electron的配置

--- electron/main.js
+++ electron/main.js
@@ -1,39 +1,40 @@
-// const { app, BrowserWindow } = require('electron');
-import { app, BrowserWindow } from 'electron';
-import { path } from 'path';
+import { app, BrowserWindow } from "electron";
+import path from "path";
+const __dirname = path.resolve();
+
 // 创建一个窗口
 const createWindow = () => {
-    // 一个window对象
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        // 配置渲染进程的环境选项
-        webPreferences: {
-            // 预加载脚本的路径
-            preload: path.join(__dirname, 'preload.js'),
-        },
-    });
-    
-    // 加载index.html
-    // win.loadFile('./electron/index.html');
+  // 一个window对象
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    // 配置渲染进程的环境选项
+    webPreferences: {
+      // 预加载脚本的路径, 先是join这个electron的目录, 然后join这个preload.js
+      preload: path.join(path.join(__dirname, "electron"), "preload.js"),
+    },
+  });
 
-    win.loadURL('http://localhost:3001');
-}
+  // 加载index.html
+  // win.loadFile('./electron/index.html');
+
+  win.loadURL("http://localhost:3001");
+};
 
 // 监听app的ready事件
 app.whenReady().then(() => {
-    createWindow();
+  createWindow();
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-    });
-})
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
 
 // 监听app的window-all-closed事件
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
